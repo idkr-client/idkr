20c56514b0a9e3e8b10c9ded7c826f18245381e9
Mixaz017 <31244102+Mixaz017@users.noreply.github.com>
2020-06-29 05:52:27 +0900

    main: Fix Resource Swapper freezing the game if there is no swap files

--- app/main.js
+++ app/main.js
@@ -58,7 +58,7 @@ function recursiveSwap(win) {
 				})
 			}
 			recursiveSwapNormal(win)
-			win.webContents.session.webRequest.onBeforeRequest({ urls: urls }, (details, callback) => { callback({ redirectURL: 'file:///' + path.join(swapDir, new URL(details.url).pathname) }) })
+			if (urls.length) win.webContents.session.webRequest.onBeforeRequest({ urls: urls }, (details, callback) => { callback({ redirectURL: 'file:///' + path.join(swapDir, new URL(details.url).pathname) }) })
 			break
 
 		case 'advanced':
@@ -71,7 +71,7 @@ function recursiveSwap(win) {
 				})
 			}
 			recursiveSwapHostname(win)
-			win.webContents.session.webRequest.onBeforeRequest({ urls: urls }, (details, callback) => {
+			if (urls.length) win.webContents.session.webRequest.onBeforeRequest({ urls: urls }, (details, callback) => {
 				let url = new URL(details.url)
 				callback({ redirectURL: 'file:///' + path.join(swapDir, url.hostname, url.pathname) })
 			})
