41f48df546310029a69f5020ecf3b3b9e55849c1
Mixaz <mixazplus@gmail.com>
2020-11-08 09:32:09 +0900

    prompt, splash: Explicitly declare HTML elements

--- app/preload/splash.js
+++ app/preload/splash.js
@@ -1,10 +1,14 @@
 const { ipcRenderer } = require('electron')
 
 document.addEventListener('DOMContentLoaded', () => {
-	ipcRenderer.invoke('get-app-info').then(info => version.innerText = `${info.name}@${info.version}`)
-})
+	const version = document.getElementById('version'),
+		message = document.getElementById('message'),
+		details = document.getElementById('details')
 
-ipcRenderer.on('message', (event, messageText = '', detailsText = '') => {
-	if (messageText != null) { message.innerText = messageText }
-	if (detailsText != null) { details.innerText = detailsText }
+	ipcRenderer.invoke('get-app-info').then(info => version.innerText = `${info.name}@${info.version}`)
+
+	ipcRenderer.on('message', (event, messageText = '', detailsText = '') => {
+		if (messageText != null) { message.innerText = messageText }
+		if (detailsText != null) { details.innerText = detailsText }
+	})
 })
