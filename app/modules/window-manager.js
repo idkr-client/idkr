c27320a790c4fe24718f85746df7f3ad7349615e
NullDev (Chris) <shadow@nulldev.org>
2021-03-06 16:44:20 +0100

    Fixed tab positioning thanks to @KraXen72

--- app/modules/window-manager.js
+++ app/modules/window-manager.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const STYLES = '#idkr-windowHolder{width:100%;height:100%;position:absolute}#idkr-menuWindow{position:absolute;left:50%;top:50%;border-radius:6px;max-height:calc(100% - 480px);transform:translate(-50%,-50%);z-index:2;overflow-y:auto;display:inline-block;text-align:left;pointer-events:auto;padding:20px;width:705px;font-size:20px;background-color:#fff;-webkit-box-shadow:0 9px 0 0 #a6a6a6;-moz-box-shadow:0 9px 0 0 #a6a6a6;box-shadow:0 9px 0 0 #a6a6a6}';
+const STYLES = '#settingsTabLayout{grid-template-columns:repeat(7, 1fr)}#idkr-windowHolder{width:100%;height:100%;position:absolute}#idkr-menuWindow{position:absolute;left:50%;top:50%;border-radius:6px;max-height:calc(100% - 480px);transform:translate(-50%,-50%);z-index:2;overflow-y:auto;display:inline-block;text-align:left;pointer-events:auto;padding:20px;width:705px;font-size:20px;background-color:#fff;-webkit-box-shadow:0 9px 0 0 #a6a6a6;-moz-box-shadow:0 9px 0 0 #a6a6a6;box-shadow:0 9px 0 0 #a6a6a6}';
 
 /**
  * Creates a new controllable PopUp Element
