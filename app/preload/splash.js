21895ef0df6b1222016e624374d0afd392a865bb
NullDev (Chris) <shadow@nulldev.org>
2021-04-18 01:04:31 +0200

    cleanup

--- app/preload/splash.js
+++ app/preload/splash.js
@@ -3,18 +3,12 @@
 const { ipcRenderer } = require('electron');
 
 document.addEventListener('DOMContentLoaded', () => {
-	const version = document.getElementById('version');
-	const message = document.getElementById('message');
-	const details = document.getElementById('details');
-
-	ipcRenderer.invoke('get-app-info').then(info => version.innerText = `${info.name}@${info.version}`);
+	ipcRenderer
+		.invoke('get-app-info')
+		.then(info => document.getElementById('version').innerText = `${info.name}@${info.version}`);
 
 	ipcRenderer.on('message', (event, messageText = '', detailsText = '') => {
-		if (messageText != null) {
-			message.innerText = messageText;
-		}
-		if (detailsText != null) {
-			details.innerText = detailsText;
-		}
+		!!messageText && (document.getElementById('message').innerText = messageText);
+		!!detailsText && (document.getElementById('details').innerText = detailsText);
 	});
 });
