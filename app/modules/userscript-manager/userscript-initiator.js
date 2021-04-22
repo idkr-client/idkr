f533df2f7fdf0f137eb0a91f62614be5b5ff7641
NullDev (Chris) <shadow@nulldev.org>
2021-04-22 22:33:16 +0200

    formulated userscript structure (wip)

--- app/modules/userscript-manager/userscript-initiator.js
+++ app/modules/userscript-manager/userscript-initiator.js
@@ -2,34 +2,93 @@
 
 let fs = require("fs");
 let path = require("path");
+let vm = require("vm");
 
-let userscriptsDirConfig = config.get("userscriptsPath", "");
-let scriptsPath = isValidPath(userscriptsDirConfig) ? userscriptsDirConfig : path.join(documentsPath, "idkr/scripts");
-try {
-	fs.readdirSync(scriptsPath).filter(filename => path.extname(filename).toLowerCase() === ".js").forEach(filename => {
+let Userscript = require("./userscript-constructor");
+let PathUtils = require("../../utils/path-utils");
+
+/**
+ * Initiate and inject usescripts
+ *
+ * @class UserscriptInitiator
+ */
+class UserscriptInitiator {
+	/**
+	 * Creates an instance of UserscriptInitiator.
+	 *
+	 * @param {object} config
+	 * @param {import("electron").App} app
+	 * @memberof UserscriptInitiator
+	 */
+	constructor(config, app){
+		let userscriptsDirConfig = config.get("userscriptsPath", "");
+		this.scriptsPath = PathUtils.isValidPath(userscriptsDirConfig)
+			? userscriptsDirConfig
+			: path.join(app.getPath("documents"), "idkr/scripts");
+	}
+
+	/**
+	 * Execution context for the scripts
+	 *
+	 * @private
+	 * @memberof UserscriptInitiator
+	 */
+	#context = {
+		console: {
+			log: (...args) => console.log(...args)
+		}
+	}
+
+	/**
+	 * Create a VM instance per userscript
+	 *
+	 * @private
+	 * @param {string} code
+	 * @memberof UserscriptInitiator
+	 */
+	#createVm = (code) => {
+		let script = new vm.Script(code);
+		vm.createContext(this.#context);
+		script.runInContext(this.#context);
+	}
+
+	/**
+	 * Inject all scripts
+	 *
+	 * @public
+	 * @param {string} windowType
+	 * @memberof UserscriptInitiator
+	 */
+	inject(windowType){
 		try {
-			let script = new Userscript(require(path.join(scriptsPath, filename)));
-			if (!script.isLocationMatching()){
-				console.log(`[USH] Ignored, location not matching: ${script.name}`);
-			}
-			else if (!script.isPlatformMatching()){
-				console.log(`[USH] Ignored, platform not matching: ${script.name}`);
-			}
-			else {
-				if (script.settings){
-					Object.assign(window._clientUtil.settings, script.settings);
+			fs.readdirSync(this.scriptsPath).filter(filename => path.extname(filename).toLowerCase() === ".js").forEach(filename => {
+				try {
+					let script = new Userscript(require(path.join(this.scriptsPath, filename)), windowType);
+					if (!script.isLocationMatching()){
+						console.log(`[USH] Ignored, location not matching: ${script.name}`);
+					}
+					else if (!script.isPlatformMatching()){
+						console.log(`[USH] Ignored, platform not matching: ${script.name}`);
+					}
+					else {
+						if (script.settings){
+							Object.assign(window._clientUtil.settings, script.settings);
+						}
+						if (script.run){
+							script.run(config);
+						}
+						console.log(`[USH] Loaded userscript: ${script.name} by ${script.author}`);
+					}
 				}
-				if (script.run){
-					script.run(config);
+				catch (err){
+					console.error("[USH] Failed to load userscript:", err);
 				}
-				console.log(`[USH] Loaded userscript: ${script.name} by ${script.author}`);
-			}
+			});
 		}
 		catch (err){
-			console.error("[USH] Failed to load userscript:", err);
+			console.error("[USH] Failed to load scripts:", err);
 		}
-	});
+	}
 }
-catch (err){
-	console.error("[USH] Failed to load scripts:", err);
-}
+
+module.exports = UserscriptInitiator;
