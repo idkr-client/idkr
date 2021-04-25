28e3f956dc86c066c902442530d87bf705dc952c
NullDev (Chris) <shadow@nulldev.org>
2021-04-25 22:26:30 +0200

    implemented clean vm subroutine

--- app/modules/userscript-manager/userscript-initiator.js
+++ app/modules/userscript-manager/userscript-initiator.js
@@ -17,14 +17,16 @@ class UserscriptInitiator {
 	 * Creates an instance of UserscriptInitiator.
 	 *
 	 * @param {object} config
-	 * @param {import("electron").App} app
+	 * @param {string} dest
+	 * @param {object} clientUtils
 	 * @memberof UserscriptInitiator
 	 */
-	constructor(config, app){
+	constructor(config, dest, clientUtils){
 		let userscriptsDirConfig = config.get("userscriptsPath", "");
 		this.scriptsPath = PathUtils.isValidPath(userscriptsDirConfig)
 			? userscriptsDirConfig
-			: path.join(app.getPath("documents"), "idkr/scripts");
+			: dest;
+		this.clientUtils = clientUtils;
 	}
 
 	/**
@@ -34,7 +36,10 @@ class UserscriptInitiator {
 	 * @memberof UserscriptInitiator
 	 */
 	#context = {
-		console: {
+		window,                         // Current Global Window
+		document,                       // Current Global Document
+		clientUtils: this.clientUtils,  // Client Utilities API
+		console: {                      // Re-bind console outside of VM
 			log: (...args) => console.log(...args)
 		}
 	}
@@ -60,33 +65,22 @@ class UserscriptInitiator {
 	 * @memberof UserscriptInitiator
 	 */
 	inject(windowType){
+		console.log("injecting...");
 		try {
 			fs.readdirSync(this.scriptsPath).filter(filename => path.extname(filename).toLowerCase() === ".js").forEach(filename => {
-				try {
-					let script = new Userscript(require(path.join(this.scriptsPath, filename)), windowType);
-					if (!script.isLocationMatching()){
-						console.log(`[USH] Ignored, location not matching: ${script.name}`);
-					}
-					else if (!script.isPlatformMatching()){
-						console.log(`[USH] Ignored, platform not matching: ${script.name}`);
-					}
-					else {
-						if (script.settings){
-							Object.assign(window._clientUtil.settings, script.settings);
-						}
-						if (script.run){
-							script.run(config);
-						}
-						console.log(`[USH] Loaded userscript: ${script.name} by ${script.author}`);
-					}
-				}
-				catch (err){
-					console.error("[USH] Failed to load userscript:", err);
-				}
+				let script = new Userscript(require(path.join(this.scriptsPath, filename)), windowType);
+				if (!script.isLocationMatching()) return console.log(`[idkr] Ignored, location not matching: ${script.name}`);
+				else if (!script.isPlatformMatching()) return console.log(`[idkr] Ignored, platform not matching: ${script.name}`);
+
+				// if (script.settings) Object.assign(window._clientUtil.settings, script.settings);
+				let executor = script.run;
+				executor && this.#createVm(String(executor));
+
+				return console.log(`[idkr] Loaded userscript: ${script.name} by ${script.author}`);
 			});
 		}
 		catch (err){
-			console.error("[USH] Failed to load scripts:", err);
+			console.error("[idkr] Failed to load scripts:", err);
 		}
 	}
 }
