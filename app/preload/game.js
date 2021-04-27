a327adb498a837de7a5f5ed5362e0ee0bfca12bf
CreepSore <eric.himmelbauer@protonmail.com>
2021-04-27 17:57:31 +0200

    Added clientUtil wrapper

--- app/preload/game.js
+++ app/preload/game.js
@@ -1,21 +1,22 @@
 "use strict";
 
 let AccountManager = require("../modules/account-manager");
+let UtilManager = require("../modules/util-manager");
 let settingsWindow = null;
 
 // @TODO: Find fix for incorrect `window` TS TypeDefs & refactor
 
-Object.assign(window._clientUtil, {
+Object.assign(window[UtilManager.instance._utilKey], {
 	searchMatches: entry => {
 		let query = settingsWindow.settingSearch.toLowerCase() || "";
 		return (entry.name.toLowerCase() || "").includes(query) || (entry.cat.toLowerCase() || "").includes(query);
 	},
 	genCSettingsHTML: options => {
 		switch (options.type){
-			case "checkbox": return `<label class='switch'><input type='checkbox' onclick='_clientUtil.setCSetting("${options.id}", this.checked)'${options.val ? " checked" : ""}><span class='slider'></span></label>`;
-			case "slider": return `<input type='number' class='sliderVal' id='c_slid_input_${options.id}' min='${options.min}' max='${options.max}' value='${options.val}' onkeypress='_clientUtil.delaySetCSetting("${options.id}", this)' style='border-width:0px'/><div class='slidecontainer'><input type='range' id='c_slid_${options.id}' min='${options.min}' max='${options.max}' step='${options.step}' value='${options.val}' class='sliderM' oninput='_clientUtil.setCSetting("${options.id}", this.value)'></div>`;
-			case "select": return `<select onchange='_clientUtil.setCSetting("${options.id}", this.value)' class='inputGrey2'>${Object.entries(options.options).map(entry => `<option value='${entry[0]}'${entry[0] === options.val ? " selected" : ""}>${entry[1]}</option>`).join("")}</select>`;
-			default: return `<input type='${options.type}' name='${options.id}' id='c_slid_${options.id}' ${options.type === "color" ? 'style="float:right;margin-top:5px;"' : `class='inputGrey2' ${options.placeholder ? `placeholder='${options.placeholder}'` : ""}`} value='${options.val.replace(/'/g, "")}' oninput='_clientUtil.setCSetting("${options.id}", this.value)'/>`;
+			case "checkbox": return `<label class='switch'><input type='checkbox' onclick='window["${UtilManager.instance._utilKey}"].setCSetting("${options.id}", this.checked)'${options.val ? " checked" : ""}><span class='slider'></span></label>`;
+			case "slider": return `<input type='number' class='sliderVal' id='c_slid_input_${options.id}' min='${options.min}' max='${options.max}' value='${options.val}' onkeypress='window["${UtilManager.instance._utilKey}"].delaySetCSetting("${options.id}", this)' style='border-width:0px'/><div class='slidecontainer'><input type='range' id='c_slid_${options.id}' min='${options.min}' max='${options.max}' step='${options.step}' value='${options.val}' class='sliderM' oninput='window["${UtilManager.instance._utilKey}"].setCSetting("${options.id}", this.value)'></div>`;
+			case "select": return `<select onchange='window["${UtilManager.instance._utilKey}"].setCSetting("${options.id}", this.value)' class='inputGrey2'>${Object.entries(options.options).map(entry => `<option value='${entry[0]}'${entry[0] === options.val ? " selected" : ""}>${entry[1]}</option>`).join("")}</select>`;
+			default: return `<input type='${options.type}' name='${options.id}' id='c_slid_${options.id}' ${options.type === "color" ? 'style="float:right;margin-top:5px;"' : `class='inputGrey2' ${options.placeholder ? `placeholder='${options.placeholder}'` : ""}`} value='${(options.val || "").replace(/'/g, "")}' oninput='window["${UtilManager.instance._utilKey}"].setCSetting("${options.id}", this.value)'/>`;
 		}
 	}
 });
@@ -28,14 +29,14 @@ let accountManager = new AccountManager();
 document.addEventListener("DOMContentLoaded", () => {
 	let windowsObserver = new MutationObserver(() => {
 		windowsObserver.disconnect();
-		window._clientUtil.events.emit("game-load");
+		UtilManager.instance.clientUtils.events.emit("game-load");
 	});
 	windowsObserver.observe(document.getElementById("instructions"), { childList: true });
 
 	accountManager.injectStyles();
 });
 
-window._clientUtil.events.on("game-load", () => {
+UtilManager.instance.clientUtils.events.on("game-load", () => {
 	window.closeClient = close;
 	settingsWindow = window.windows[0];
 
@@ -48,8 +49,8 @@ window._clientUtil.events.on("game-load", () => {
 		if (clientTabIndex !== settingsWindow.tabIndex + 1 && !settingsWindow.settingSearch) return "";
 		let tempHTML = "";
 		let previousCategory = null;
-		Object.values(window._clientUtil.settings).forEach(entry => {
-			if (settingsWindow.settingSearch && !window._clientUtil.searchMatches(entry) || entry.hide) {
+		Object.values(UtilManager.instance.clientUtils.settings).forEach(entry => {
+			if (settingsWindow.settingSearch && !UtilManager.instance.clientUtils.searchMatches(entry) || entry.hide) {
 				return;
 			}
 			if (previousCategory !== entry.cat) {
