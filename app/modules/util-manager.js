67e48176793d75789f4950742918cdca4c8bcc6c
NullDev (Chris) <shadow@nulldev.org>
2021-04-27 22:03:56 +0200

    someone doesn't know how linter work

--- app/modules/util-manager.js
+++ app/modules/util-manager.js
@@ -1,5 +1,17 @@
+"use strict";
+
+/**
+ * Global clientUtil manager
+ *
+ * @class UtilManager
+ */
 class UtilManager {
-	constructor() {
+	/**
+	 * Creates an instance of UtilManager.
+	 *
+	 * @memberof UtilManager
+	 */
+	constructor(){
 		this._utilKey = `${UtilManager.randomString(10)}`;
 	}
 
@@ -9,8 +21,8 @@ class UtilManager {
 	 * @static
 	 * @memberof UtilManager
 	 */
-	static get instance() {
-		return this._instance = this._instance || new UtilManager();
+	static get instance(){
+		return (this._instance = this._instance || new UtilManager());
 	}
 
 	/**
@@ -23,20 +35,22 @@ class UtilManager {
 	/**
 	 * @memberof UtilManager
 	 */
-	set clientUtils(value) {
+	set clientUtils(value){
 		window[this._utilKey] = value;
 	}
 
 	/**
-	 * @param {number} length length of the random string
-	 * @param {string[]} validChars array of valid chars or sequences
+	 * Generate a random string for the global util attribute
+	 *
+	 * @param {number} length - length of the random string
+	 * @param {string[]} validChars - array of valid chars or sequences
 	 * @returns {string} random string of specified length
 	 */
-	static randomString(length, validChars = [...'abcdefghijklmnopqrstuvwxyz1234567890']) {
+	static randomString(length, validChars = [..."abcdefghijklmnopqrstuvwxyz1234567890"]){
 		return Array(length)
 			.fill(null)
-			.map(() => validChars[Math.floor(Math.random()*validChars.length)])
-			.join('');
+			.map(() => validChars[Math.floor(Math.random() * validChars.length)])
+			.join("");
 	}
 }
 
