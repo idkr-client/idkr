32dfe7f468321db035e39a36a65472379d66a8ce
NullDev <shadow@nulldev.org>
2021-05-25 16:01:58 +0200

    added menu timer stuff

--- app/modules/styles-injection.js
+++ app/modules/styles-injection.js
@@ -37,4 +37,50 @@ module.exports = `
 	-moz-box-shadow: 0 9px 0 0 #a6a6a6;
 	box-shadow: 0 9px 0 0 #a6a6a6
 }
+
+#aHolder {
+	display:none !important;
+}
+
+#spectateUI {
+	display: block !important;
+}
+
+#specNames, #specGameInfo, #spec0, #spectateInfo, #spec1, #specStats, #specSUS, #specContr, #specKPDContr {
+	display: none !important;
+}
+
+#specTimer {
+	font-size: 28px;
+	text-align: center;
+	position: fixed;
+	transform: translateX(-50%);
+	left: 50%;
+	top: 20px;
+	width: 221px;
+	height: 89px;
+	padding: 0;
+	margin:0;
+	border-radius: 0;
+	background-color: transparent;
+	will-change: unset;
+	text-shadow: 2px 2px 3px rgba(30, 30, 30, .5);
+}
+
+#spectateHUD {
+	transform:translate(-50%,-50%);
+	position:fixed;
+	top:55%;
+}
+
+#uiBase.onMenu #specTimer {
+	display:block !important;
+	color:red;
+	background-color:transparent;
+	opacity: 1;
+}
+
+#instructionHolder {
+	z-index: -2;
+}
 `;
