idkr-client/idkr

commit 97c00a228265b986d31e40921fa2119f5e84421a

Semantic refactoring & typing

app/preload/global.js
@@ -3,6 +3,7 @@
 let path = require("path");
 
 require("v8-compile-cache");
+
 let events = require("events");
 let { ipcRenderer } = require("electron");
 let Store = require("electron-store");
@@ -24,34 +25,25 @@ window._clientUtil = {
 	settings: require("../exports/settings"),
 	setCSetting(name, value) {
 		let entry = Object.values(this.settings).find(entry => entry.id === name);
-		if (entry.min && entry.max){
-			value = Math.max(entry.min, Math.min(value, entry.max));
-		}
+		if (entry.min && entry.max) value = Math.max(entry.min, Math.min(value, entry.max));
+
 		config.set(name, value);
 		entry.val = value;
-		if (entry.set) {
-			entry.set(value);
-		}
+		if (entry.set) entry.set(value);
+
 		let element = document.getElementById("c_slid_" + entry.id);
-		if (element) {
-			element.value = value;
-		}
+
+		if (element) element.value = value;
 		element = document.getElementById("c_slid_input_" + entry.id);
-		if (element) {
-			element.value = value;
-		}
+		if (element) element.value = value;
 	},
 	delayIDs: {},
 	delaySetCSetting(name, target, delay = 600) {
-		if (this.delayIDs[name]) {
-			clearTimeout(this.delayIDs[name]);
-		}
+		if (this.delayIDs[name]) clearTimeout(this.delayIDs[name]);
 		this.delayIDs[name] = setTimeout(() => {
 			this.setCSetting(name, target.value);
 			delete this.delayIDs[name];
 		}, delay);
-	},
-	loadScripts(){
 	},
 	initUtil(){
 		for (let [key, entry] of Object.entries(this.settings)) {
@@ -64,19 +56,13 @@ window._clientUtil = {
 				delete this.settings[key];
 				continue;
 			}
-			if (entry.dontInit) {
-				continue;
-			}
+			if (entry.dontInit) continue;
+
 			let savedVal = config.get(entry.id);
-			if (savedVal !== null) {
-				entry.val = savedVal;
-			}
-			if (entry.min && entry.max) {
-				entry.val = Math.max(entry.min, Math.min(entry.val, entry.max));
-			}
-			if (entry.set) {
-				entry.set(entry.val, true);
-			}
+
+			if (savedVal !== null) entry.val = savedVal;
+			if (entry.min && entry.max) entry.val = Math.max(entry.min, Math.min(entry.val, entry.max));
+			if (entry.set) entry.set(entry.val, true);
 		}
 	}
 };
@@ -94,9 +80,9 @@ switch (windowType) {
 
 let rpcIntervalId;
 
-function setFocusEvent() {
+function setFocusEvent(){
 	window.addEventListener("focus", () => {
-		function sendRPCGamePresence() {
+		function sendRPCGamePresence(){
 			try {
 				let gameActivity = window.getGameActivity();
 
@@ -105,9 +91,7 @@ function setFocusEvent() {
 					details: gameActivity.mode
 				});
 
-				if (gameActivity.time) {
-					rpcActivity.endTimestamp = Date.now() + gameActivity.time * 1e3;
-				}
+				if (gameActivity.time) rpcActivity.endTimestamp = Date.now() + gameActivity.time * 1e3;
 				ipcRenderer.invoke("rpc-activity", rpcActivity);
 			}
 			catch (error) {
@@ -124,14 +108,13 @@ function setFocusEvent() {
 		};
 		let isIntervalSet = false;
 		switch (windowType) {
-			case "game":
+			case "game": {
 				sendRPCGamePresence();
-				if (rpcIntervalId) {
-					clearInterval(rpcIntervalId);
-				}
+				if (rpcIntervalId) clearInterval(rpcIntervalId);
 				rpcIntervalId = setInterval(sendRPCGamePresence, 5e3);
 				isIntervalSet = true;
 				break;
+			}
 			case "docs":
 				rpcActivity.state = "Reading Docs";
 				break;
@@ -159,9 +142,7 @@ function setFocusEvent() {
 
 ipcRenderer.on("rpc-stop", () => {
 	setFocusEvent();
-	if (rpcIntervalId) {
-		clearInterval(rpcIntervalId);
-	}
+	if (rpcIntervalId) clearInterval(rpcIntervalId);
 });
 
 ipcRenderer

app/preload/prompt.js
@@ -2,10 +2,6 @@
 
 let { ipcRenderer } = require("electron");
 
-/**
- * @typedef {Window & any} window
- */
-
 window.addEventListener("DOMContentLoaded", () => {
 	/** @type {HTMLInputElement} */
 	const promptInput = (document.getElementById("promptInput"));
@@ -19,17 +15,12 @@ window.addEventListener("DOMContentLoaded", () => {
 		promptInput.focus();
 	});
 
-	/** @type {window} */
+	/** @type {Window & any} */
 	(window).sendValue = value => {
 		ipcRenderer.send("prompt-return", value);
 		window.close();
 	};
 
-	/** @type {window} */
-	(window).importFile = () => {
-		/**
-		 * @type {HTMLInputElement}
-		 */
-		(document.getElementById("fileSelect")).files[0].text().then(text => (promptInput.value = text));
-	};
+	// @ts-ignore
+	window.importFile = () => (document.getElementById("fileSelect").files[0].text().then(text => (promptInput.value = text)));
 });

commit 70b72bd6c9be15e317274886ddea45ee0d45c8bb

Fix client settings being undef if not saved in config
Another bug introduced by Null in 97c00a2
...and somehow no one noticed
This bug basically made everything turned off in settings menu

app/preload/global.js
@@ -62,7 +62,7 @@ UtilManager.instance.clientUtils = {
 			}
 			if (entry.dontInit) continue;
 
-			let savedVal = config.get(entry.id);
+			let savedVal = config.get(entry.id, null);
 
 			if (savedVal !== null) entry.val = savedVal;
 			if (entry.min && entry.max) entry.val = Math.max(entry.min, Math.min(entry.val, entry.max));