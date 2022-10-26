d460b0963c84cb1953283912c30936f034588fc8
prljav <74116121+prljav@users.noreply.github.com>
2022-10-26 21:10:09 +0200

    bump just to finalyl github build

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 npm install --openssl_fips=''
 #legit no1 knows if --openssl_fips down below is needed?????? if u want make pr and remove if safe?
+#wow u really found this
 npm run dist --openssl_fips=''
